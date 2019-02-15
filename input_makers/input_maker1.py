1728babbf4067795451d4b1d0df7e4829c35fd72
navidkpr <navidkouchakipour@yahoo.com>
2019-02-15 13:03:26 -0500

    Added lab 5 parts 1 and 2

---
+++ input_makers/input_maker1.py
@@ -0,0 +1,14 @@
+import random
+
+test_num = 2;
+
+def make_inputs():
+    for x in range(test_num):
+        myfile = open("inputs/input{0}.in".format(x), 'w');
+        myfile.write(str(random.randrange(0, 100000000)));
+
+def main():
+    make_inputs()
+
+if __name__== "__main__":
+    main()
---
+++ input_makers/input_maker3.py
@@ -0,0 +1,17 @@
+import random
+
+test_num = 100
+
+def make_inputs():
+    for x in range(test_num):
+        myfile = open("inputs/input{0}.in".format(x), 'w')
+        date = random.randrange(1, 30)
+        month = random.randrange(1, 12)
+        year = random.randrange(1000, 3000)
+        myfile.write(('0' if date < 10 else '') + str(date) + '/' + ('0' if month<10 else '') + str(month) + '/' + str(year))
+
+def main():
+    make_inputs()
+
+if __name__ == '__main__':
+    main()
